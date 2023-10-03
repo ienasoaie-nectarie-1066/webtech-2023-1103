a418ab7bada45d5d6eca9d623a7b6c99ff5937c9
ienasoaie-nectarie-1066 <ienasoaienectarie21@stud.ase.ro>
2023-10-03 15:47:19 +0300

    ex. 5 modif

--- wk2/5.js
+++ wk2/5.js
@@ -8,3 +8,13 @@ console.log(add(1,2)) //NaN
 console.log(add(1,"man",3)) //concatenates
 
 let sampleArray = [1,2,3]
+
+function buildArray(source, ...args){
+    //js version of foreach, using "of"
+    for(let element of args){
+        source.push(element)
+    }
+    return source
+}
+
+console.log(buildArray([1,2,3],4,5,6,7,8))
