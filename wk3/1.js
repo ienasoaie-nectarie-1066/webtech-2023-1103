cbb4db8b84b39aac602806f6b57097a6699faecf
ienasoaie-nectarie-1066 <ienasoaienectarie21@stud.ase.ro>
2023-10-09 14:03:34 +0300

    update 1

--- wk3/1.js
+++ wk3/1.js
@@ -13,12 +13,3 @@ function getLongerThan(a, n){
     return a.filter(e => e.length>n)
 }
 console.log(getLongerThan(sampleStrings, 3))
-
-const yearsOfBirth = [1996, 2003, 2012, 1987, 2001, 2020]
-function getMajor(a1){
-    return a1.filter(e => 2023 - e >= 18)
-}
-console.log(getMajor(yearsOfBirth))
-
-const results = yearsOfBirth.map(e => 2023 - e).filter(e => e>=18)
-console.log(results)
