c92eedda075e87e75b651fb619318934699d6650
ienasoaie-nectarie-1066 <ienasoaienectarie21@stud.ase.ro>
2023-10-09 14:15:48 +0300

    removed excess in 3

--- wk3/3.js
+++ wk3/3.js
@@ -2,7 +2,3 @@ const sampleArray = [1, 5, 9, 6, 8]
 const totalArea = sampleArray.map(e => e*e).reduce((a,e)=> a+e, 0)
 //produces the sum of the squares of sampleArray
 console.log(totalArea)
-function sumOfDiv(a, d){
-    return result = a.map(e => e%d == 0).reduce((t,e)=> t+e, 0)
-}
-console.log(sumOfDiv(sampleArray, 3))
