1295683ed765440a39d5cd9a97d0e7855bc6d99b
ienasoaie-nectarie-1066 <ienasoaienectarie21@stud.ase.ro>
2023-10-11 16:46:37 +0300

    renamed folder

---
+++ hw1/app.js
@@ -0,0 +1,29 @@
+function distance(first, second){
+    if(typeof first != typeof [] || typeof second != typeof []){
+        throw new TypeError('InvalidType')
+    }
+    first = first.reduce(function(a,b){
+        if(a.indexOf(b) < 0){
+            a.push(b)
+        }
+        return a
+    },[])
+    second = second.reduce(function(a,b){
+        if(a.indexOf(b) < 0){
+            a.push(b)
+        }
+        return a
+    },[])
+    //return second
+    let cntr = 0
+    for(let i = 0; i < first.length; i++){
+        if(second.indexOf(first[i]) >= 0){
+            cntr++
+        } 
+    }
+    return first.length + second.length - cntr*2
+}
+console.log(distance(['a','a','a'], ['a','c','d','c']))
+//------------------------['a']--------['a','c','d']---
+//--------------------------1----------------3---------
+module.exports.distance = distance
