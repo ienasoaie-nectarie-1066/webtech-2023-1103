ab7f851434818a8f6c1de88e392259ff79a65c14
ienasoaie-nectarie-1066 <ienasoaienectarie21@stud.ase.ro>
2023-10-16 20:44:55 +0300

    ex.5; custom errors with try/catch block

---
+++ wk4/5.js
@@ -0,0 +1,21 @@
+//throwing custom errors
+const increaseSalary = (salaries, percentage) =>{
+    if(typeof salaries !== typeof []){
+        throw new Error('Salaries must come in an array')
+    }
+    if(typeof percentage !== typeof 1){
+        throw new Error('Percentage must be a number')
+    }
+    for(let i=0; i<salaries.length; i++){
+        salaries[i] = salaries[i] + salaries[i]*percentage/100
+    }
+    console.log(salaries)
+}
+//using the try/catch block
+try{
+    increaseSalary([100,200,300],50)
+    //increaseSalary(100,10)
+    //increaseSalary([100,200,300],'a')
+}catch(err){
+    console.warn(err)
+}
