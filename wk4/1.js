73be48279a2f0a550dccb3b4db6d73a2e14ef83f
ienasoaie-nectarie-1066 <ienasoaienectarie21@stud.ase.ro>
2023-10-16 20:52:53 +0300

    added relevant comments

--- wk4/1.js
+++ wk4/1.js
@@ -1,7 +1,7 @@
 class Stream{
     #value;
     #nextValue;
-    static #count = 0;
+    static #count = 0; //counts number of streams (children) Stream has
     constructor(value, nextValue){
         this.#value = value;
         this.#nextValue = nextValue;
@@ -18,30 +18,32 @@ class Stream{
         return Stream.#count;
     }
 }
-
+//constant value
 class ConstantStream extends Stream{
     constructor(value){
         super(value, value=>value);
 
     }
 }
-
+//value increasing by 1
 class NextIntegerStream extends Stream{
     constructor(){
         super(0, value => value+1);
     }
 }
-
+//initialising objects
 const constant = new ConstantStream(1);
 const nextInteger = new NextIntegerStream();
 
 console.log(Stream.count)
 //my object type
+//even numbers
 class evenIntegers extends Stream{
     constructor(value){
         super(value, value=>value+2)
     }
 }
+//initialising with 0
 const eventInteger = new evenIntegers(0)
 for(let i=0; i<10; i++){
     console.log(`constant [${i}] = ${constant.next}`);
