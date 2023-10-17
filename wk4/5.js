22b44ef79dce9e6d3ae42841beeed84a2278aca6
ienasoaie-nectarie-1066 <ienasoaienectarie21@stud.ase.ro>
2023-10-17 15:42:28 +0300

    modified the function

--- wk4/5.js
+++ wk4/5.js
@@ -6,14 +6,15 @@ const increaseSalary = (salaries, percentage) =>{
     if(typeof percentage !== typeof 1){
         throw new Error('Percentage must be a number')
     }
-    for(let i=0; i<salaries.length; i++){
+   /* for(let i=0; i<salaries.length; i++){
         salaries[i] = salaries[i] + salaries[i]*percentage/100
-    }
+    }*/
+    salaries = salaries.map(e=>e+e*percentage/100)
     console.log(salaries)
 }
 //using the try/catch block
 try{
-    increaseSalary([100,200,300],50)
+    increaseSalary([100,200,300],1)
     //increaseSalary(100,10)
     //increaseSalary([100,200,300],'a')
 }catch(err){
